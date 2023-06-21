4be78549c86362158c94efd6d2c80b104d1d2787
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-06-21 04:45:10 -0300

    modified prompt script

--- prompt.py
+++ prompt.py
@@ -1,5 +1,6 @@
 import os
 import time
+import json
 
 import openai
 
@@ -38,18 +39,35 @@ def summarize(chunks):
             Generar un resumen del documento incluyendo los datos antes \
             mencionados. El mensaje debe ser informativo, detallado, \ 
             pero conciso y orientado a un lector promedio sin conocimientos \
-            en leyes.
+            en leyes. Si alguno de los puntos relevantes no está presente, \
+            simplemente omitirlo y no nombrarlo en absoluto. Por ejemplo, \
+            si no se nombra dinero involucrado, no se hará mención del tema \
+            en la respuesta.
             No hace falta un texto introductorio aclarando que se  \
             trata de un resumen y obviar detalles como la ciudad \
             en la que sucede, y que se trata de una norma.
+            Las instituciones no deben ser el centro del mensaje, y deben \
+            ser nombradas lo mínimo posible. Tampoco debe incluirse la fecha \
+            de publicación ni que fue publicada en el Boletín Oficial.
+            Los comentarios del estilo 'el texto no hace mención al ciudadano \
+            común' y/o 'el texto no trata sobre dinero' serán eliminados \
+            de la respuesta.
             El texto generado debe ser arreglado como una noticia, \
             y deberá cumplir con todos los requisitos necesarios para su \
-            uso como tweet diario. Ser muy corto, preciso, informativo, \
-            y generar engagement.
-            Review: ```{chunks[0]}```
+            uso como tweet diario. Debe ser corto, preciso, informativo, \
+            y generar engagement a través de emogis y pocos hashtags \
+            dependiendo el caso.
+            El formato de salida será json con la siguiente \
+            estructura:
+            "date": "fecha_de_publicacion", "summary": "respuesta_generada"
+            donde fecha_de_publicacion es la fecha de publicacion del articulo y 'summary' \
+            es respuesta_generada, la respuesta generada anteriormente.
+            Texto: ```{chunks[0]}```
         """
         response = get_completion(prompt)
+
     else:
+        print(f"{len(chunks)} chunks. Please wait!")
         initial_response = ''
         for i, chunk in enumerate(chunks):
             prompt = f"""
@@ -71,17 +89,20 @@ def summarize(chunks):
                 mencionados. El mensaje debe ser informativo, detallado, \ 
                 pero conciso y orientado a un lector promedio sin conocimientos \
                 en leyes.
+                Si alguno de los puntos relevantes no está presente, \
+                simplemente omitirlo y no nombrarlo.
                 No hace falta un texto introductorio aclarando que se  \
                 trata de un resumen y obviar detalles como la ciudad \
                 en la que sucede, y que se trata de una norma.
                 El texto generado debe ser arreglado como una noticia, \
                 y deberá cumplir con todos los requisitos necesarios para su \
-                uso como tweet diario. Ser muy corto, preciso, informativo, \
+                uso como tweet diario. Debe ser corto, preciso, informativo, \
                 y generar engagement.
                 Review: ```{chunk}```
             """
             initial_response += get_completion(prompt)
             time.sleep(20)
+            print(f"{i+1} done.")
 
         prompt = f"""
             Extraer la siguiente información relevante del texto \ 
@@ -99,16 +120,34 @@ def summarize(chunks):
             Generar un resumen del documento incluyendo los datos antes \
             mencionados. El mensaje debe ser informativo, detallado, \ 
             pero conciso y orientado a un lector promedio sin conocimientos \
-            en leyes.
+            en leyes. Si alguno de los puntos relevantes no está presente, \
+            simplemente omitirlo y no nombrarlo en absoluto. Por ejemplo, \
+            si no se nombra dinero involucrado, no se hará mención del tema \
+            en la respuesta.
             No hace falta un texto introductorio aclarando que se  \
             trata de un resumen y obviar detalles como la ciudad \
             en la que sucede, y que se trata de una norma.
+            Las instituciones no deben ser el centro del mensaje, y deben \
+            ser nombradas lo mínimo posible. Tampoco debe incluirse la fecha \
+            de publicación ni que fue publicada en el Boletín Oficial.
+            Los comentarios del estilo 'el texto no hace mención al ciudadano \
+            común' y/o 'el texto no trata sobre dinero' serán eliminados \
+            de la respuesta.
             El texto generado debe ser arreglado como una noticia, \
             y deberá cumplir con todos los requisitos necesarios para su \
-            uso como tweet diario. Ser muy corto, preciso, informativo, \
-            y generar engagement.
+            uso como tweet diario. Debe ser corto, preciso, informativo, \
+            y generar engagement a través de emogis y pocos hashtags \
+            dependiendo el caso.
+            El formato de salida será json con la siguiente \
+            estructura:
+            "date": fecha_de_publicacion, "summary": respuesta_generada
+            donde fecha_de_publicacion es la fecha de publicacion del articulo y 'summary' \
+            es respuesta_generada, la respuesta generada anteriormente.
             Review: ```{initial_response}```
         """
-        response = get_completion(prompt)        
-
-    return response
+        response = get_completion(prompt)  
+    print(response)      
+    try:
+        return json.loads(response)
+    except:
+        return json.loads('{'+response+'}')
