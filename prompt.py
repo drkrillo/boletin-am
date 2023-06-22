4fddb61beb387e74318196c9c03523349fb9b8ca
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-06-22 03:59:54 -0300

    modified script to extract features

--- prompt.py
+++ prompt.py
@@ -1,13 +1,12 @@
 import os
 import time
-import json
 
 import openai
 
 from dotenv import load_dotenv
 load_dotenv()
 
-
+delimiter = "####"
 MAX_TOKENS = 4096
 openai.api_key  = os.getenv('OPENAI_API_KEY')
 
@@ -24,45 +23,43 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
 def summarize(chunks):
     if len(chunks) <= 1:
         prompt = f"""
-            Extraer la siguiente información relevante del texto \ 
-            jurídico entre triple comillas manteniendo los detalles \
-            relevantes a dinero, nombres, y la importancia para la  \
-            ciudadanía sobre quienes aplica.
+            Seguir los siguientes pasos para generar un tweet del resumen del \
+            texto jurídico del Boletín Oficial de Argentina delimitado \
+            por cuatro hastags, {delimiter}.
 
-            Información a extraer:
-            Fecha de publicación,
-            Instituciones involucradas,
-            Cargos o personas involucradas,
-            Potenciales impactos sobre el común ciudadano,
-            Datos relativos a dinero que implica el documento.
+            Paso 1: {delimiter} Primero, responder: ¿Es el texto \
+            relevante desde un punto de vista social? Responder \
+            sólo con una palabra: Si / No
+            Paso 2: {delimiter} Segundo, responder: ¿Es el texto \
+            relevante desde un punto de vista económico? Responder \
+            sólo con una palabra: Si / No
+            Paso 3: {delimiter} Tercero, responder: ¿Es el texto \
+            relevante desde un punto de vista ecológico y \
+            sustentable? Responder sólo con una palabra: Si / No
+            Paso 4: {delimiter} Cuarto, responder: ¿Es el texto \
+            relevante desde un punto de vista político? Responder \
+            sólo con una palabra: Si / No                
 
-            Generar un resumen del documento incluyendo los datos antes \
-            mencionados. El mensaje debe ser informativo, detallado, \ 
+            Generar un resumen del documento. El mensaje debe ser informativo, detallado, \ 
             pero conciso y orientado a un lector promedio sin conocimientos \
-            en leyes. Si alguno de los puntos relevantes no está presente, \
-            simplemente omitirlo y no nombrarlo en absoluto. Por ejemplo, \
-            si no se nombra dinero involucrado, no se hará mención del tema \
-            en la respuesta.
+            en leyes.
             No hace falta un texto introductorio aclarando que se  \
-            trata de un resumen y obviar detalles como la ciudad \
-            en la que sucede, y que se trata de una norma.
-            Las instituciones no deben ser el centro del mensaje, y deben \
-            ser nombradas lo mínimo posible. Tampoco debe incluirse la fecha \
-            de publicación ni que fue publicada en el Boletín Oficial.
-            Los comentarios del estilo 'el texto no hace mención al ciudadano \
-            común' y/o 'el texto no trata sobre dinero' serán eliminados \
-            de la respuesta.
+            trata de un resumen.
+            No debe incluirse la fecha de publicación ni que fue \
+            publicada en el Boletín Oficial.
             El texto generado debe ser arreglado como una noticia, \
             y deberá cumplir con todos los requisitos necesarios para su \
             uso como tweet diario. Debe ser corto, preciso, informativo, \
-            y generar engagement a través de emogis y pocos hashtags \
-            dependiendo el caso.
-            El formato de salida será json con la siguiente \
-            estructura:
-            "date": "fecha_de_publicacion", "summary": "respuesta_generada"
-            donde fecha_de_publicacion es la fecha de publicacion del articulo y 'summary' \
-            es respuesta_generada, la respuesta generada anteriormente.
-            Texto: ```{chunks[0]}```
+            y generar engagement a través de pocos hashtags.
+
+            Usar el siguiente formato:
+            Paso 1:{delimiter} <Si/No Paso 1>
+            Paso 2:{delimiter} <Si/No Paso 2>
+            Paso 3:{delimiter} <Si/No Paso 3>
+            Paso 4:{delimiter} <Si/No Paso 4>
+            Tweet:{delimiter} <Tweet con resumen del artículo>
+
+            Texto:{delimiter}{chunks[0]}{delimiter}
         """
         response = get_completion(prompt)
 
@@ -71,83 +68,90 @@ def summarize(chunks):
         initial_response = ''
         for i, chunk in enumerate(chunks):
             prompt = f"""
-                Extraer la siguiente información relevante del texto \ 
-                jurídico entre triple comillas manteniendo los detalles \
-                relevantes a dinero, nombres, y la importancia para la  \
-                ciudadanía sobre quienes aplica.
-                Tener en cuenta que es el trozo {i+1} de {len(chunks)+1}. \
+                Resumir el fragmento de un artículo publicado en el Boletín \
+                Oficial de Argentina delimitado por cuatro hastags, {delimiter}.
+                Tener en cuenta que es el trozo {i+1} de {len(chunks)+1} trozos. \
                 Luego, todos estos trozos serán unificados en el mismo mensaje.
 
-                Información a extraer:
-                Fecha de publicación,
-                Instituciones involucradas,
-                Cargos o personas involucradas,
-                Potenciales impactos sobre el común ciudadano,
-                Datos relativos a dinero que implica el documento.
-
-                Generar un resumen del documento incluyendo los datos antes \
-                mencionados. El mensaje debe ser informativo, detallado, \ 
-                pero conciso y orientado a un lector promedio sin conocimientos \
-                en leyes.
-                Si alguno de los puntos relevantes no está presente, \
-                simplemente omitirlo y no nombrarlo.
-                No hace falta un texto introductorio aclarando que se  \
-                trata de un resumen y obviar detalles como la ciudad \
-                en la que sucede, y que se trata de una norma.
-                El texto generado debe ser arreglado como una noticia, \
-                y deberá cumplir con todos los requisitos necesarios para su \
-                uso como tweet diario. Debe ser corto, preciso, informativo, \
-                y generar engagement.
-                Review: ```{chunk}```
+                Review: {delimiter}{chunk}{delimiter}
             """
             initial_response += get_completion(prompt)
             time.sleep(20)
             print(f"{i+1} done.")
 
         prompt = f"""
-            Extraer la siguiente información relevante del texto \ 
-            jurídico entre triple comillas manteniendo los detalles \
-            relevantes a dinero, nombres, y la importancia para la  \
-            ciudadanía sobre quienes aplica.
+            Seguir los siguientes pasos para generar un tweet del resumen del \
+            texto jurídico del Boletín Oficial de Argentina delimitado \
+            por cuatro hastags, {delimiter}.
 
-            Información a extraer:
-            Fecha de publicación,
-            Instituciones involucradas,
-            Cargos o personas involucradas,
-            Potenciales impactos sobre el común ciudadano,
-            Datos relativos a dinero que implica el documento.
+            Paso 1: {delimiter} Primero, responder: ¿Es el texto \
+            relevante desde un punto de vista social? Responder \
+            sólo con una palabra: Si / No
+            Paso 2: {delimiter} Segundo, responder: ¿Es el texto \
+            relevante desde un punto de vista económico? Responder \
+            sólo con una palabra: Si / No
+            Paso 3: {delimiter} Tercero, responder: ¿Es el texto \
+            relevante desde un punto de vista ecológico y \ 
+            sustentable? Responder sólo con una palabra: Si / No
+            Paso 4: {delimiter} Cuarto, responder: ¿Es el texto \
+            relevante desde un punto de vista político? Responder \
+            sólo con una palabra: Si / No      
 
-            Generar un resumen del documento incluyendo los datos antes \
-            mencionados. El mensaje debe ser informativo, detallado, \ 
+            Generar un resumen del documento. El mensaje debe ser informativo, detallado, \ 
             pero conciso y orientado a un lector promedio sin conocimientos \
-            en leyes. Si alguno de los puntos relevantes no está presente, \
-            simplemente omitirlo y no nombrarlo en absoluto. Por ejemplo, \
-            si no se nombra dinero involucrado, no se hará mención del tema \
-            en la respuesta.
+            en leyes.
             No hace falta un texto introductorio aclarando que se  \
-            trata de un resumen y obviar detalles como la ciudad \
-            en la que sucede, y que se trata de una norma.
-            Las instituciones no deben ser el centro del mensaje, y deben \
-            ser nombradas lo mínimo posible. Tampoco debe incluirse la fecha \
-            de publicación ni que fue publicada en el Boletín Oficial.
-            Los comentarios del estilo 'el texto no hace mención al ciudadano \
-            común' y/o 'el texto no trata sobre dinero' serán eliminados \
-            de la respuesta.
+            trata de un resumen.
+            No debe incluirse la fecha de publicación ni que fue \
+            publicada en el Boletín Oficial.
             El texto generado debe ser arreglado como una noticia, \
             y deberá cumplir con todos los requisitos necesarios para su \
             uso como tweet diario. Debe ser corto, preciso, informativo, \
-            y generar engagement a través de emogis y pocos hashtags \
-            dependiendo el caso.
-            El formato de salida será json con la siguiente \
-            estructura:
-            "date": fecha_de_publicacion, "summary": respuesta_generada
-            donde fecha_de_publicacion es la fecha de publicacion del articulo y 'summary' \
-            es respuesta_generada, la respuesta generada anteriormente.
-            Review: ```{initial_response}```
+            y generar engagement a través de pocos hashtags.
+
+            Usar el siguiente formato:
+            Paso 1:{delimiter} <Razonamiento Paso 1>
+            Paso 2:{delimiter} <Razonamiento Paso 2>
+            Paso 3:{delimiter} <Razonamiento Paso 3>
+            Paso 4:{delimiter} <Razonamiento Paso 4>
+            Tweet:{delimiter} <Tweet con resumen del artículo>
+
+            Texto:{delimiter}{initial_response}{delimiter}
         """
-        response = get_completion(prompt)  
-    print(response)      
-    try:
-        return json.loads(response)
-    except:
-        return json.loads('{'+response+'}')
+        response = get_completion(prompt)
+
+    social, rest = response.split('\n', 1)
+    social = int(social.split(delimiter)[1]
+                 .replace('No', '0')
+                 .replace('Sí', '1')
+                 .replace('Si', '1')
+                 .strip()
+    )
+
+    economic, rest = rest.split('\n', 1)
+    economic = int(economic.split(delimiter)[1]
+                   .replace('No', '0')
+                   .replace('Sí', '1')
+                   .replace('Si', '1')
+                   .strip()
+    )
+
+    sustainable, rest = rest.split('\n', 1)
+    sustainable = int(sustainable.split(delimiter)[1]
+                      .replace('No', '0')
+                      .replace('Sí', '1')
+                      .replace('Si', '1')
+                      .strip()
+    )
+
+    politic, rest = rest.split('\n', 1)
+    politic = int(politic.split(delimiter)[1]
+                  .replace('No', '0')
+                  .replace('Sí', '1')
+                  .replace('Si', '1')
+                  .strip()
+    )
+
+    tweet = rest.split(delimiter)[1].strip()
+
+    return social, economic, sustainable, politic, tweet
