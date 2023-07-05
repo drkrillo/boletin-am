dc84b8566f8ee17a7c3563ca09479702232a7104
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-07-05 15:10:43 -0300

    added pipeline for publication

---
+++ utils/generate_publication.py
@@ -0,0 +1,31 @@
+from datetime import date
+import time
+import pandas as pd
+
+import scraper
+import preprocessing
+import prompt
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def generate_publication(id):
+    url = f'https://www.boletinoficial.gob.ar/detalleAviso/primera/{id}/20230704'
+    type, content = scraper.scrape(url)
+
+    chunks = preprocessing.chop(content)
+    tags, score, summary = prompt.summarize(chunks)
+    date = content[-11:].replace('\n','')
+
+    publication = {
+        id: id,
+        date: date,
+        url: url,
+        type: type,
+        summary: summary,
+        tags: tags,
+        score: score
+    }
+
+    return publication
