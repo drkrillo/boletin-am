c3b25bf5ee870ec3f7c0fad52eb94b90e2f21bf4
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-07-12 16:54:44 -0300

    testing new funcs

--- app/testing_pipeline.py
+++ app/testing_pipeline.py
@@ -1,15 +1,7 @@
 import json
 from utils.generate_publication import generate_publication
+from utils.scraper import *
 
-id = 289141
-fn = '../data.json'
+urls = today_urls()
 
-while True:
-    publication = generate_publication(id)
-
-    with open(fn, "r+") as file:
-        data = json.load(file)
-        data.append(publication)
-        file.seek(0)
-        json.dump(data, file)
-    id += 1
+print(urls)
