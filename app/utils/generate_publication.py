af6a665b7a9a9aa71614e4f99d7945611797bdd5
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-07-12 18:42:35 -0300

    modified script to use new funcs

--- app/utils/generate_publication.py
+++ app/utils/generate_publication.py
@@ -7,15 +7,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def generate_publication(id):
-    url = f'https://www.boletinoficial.gob.ar/detalleAviso/primera/{id}/20230704'
-    type, content = scraper.scrape(url)
+def generate_publication(url):
+    type, content, date = scraper.scrape_article(url)
     print('Completed Scraping')
+
     chunks = preprocessing.chop(content)
     print('Completed Preprocessing')
+
     tags, score, summary = prompt.summarize(chunks)
     print('Completed Extraction')
-    date = preprocessing.transform_date(content[-11:].replace('\n',''))
+
     publication = {
         'id': id,
         'date': date,
@@ -27,4 +28,5 @@ def generate_publication(id):
     }
     print('Publicated Created')
     print(publication)
+
     return publication
