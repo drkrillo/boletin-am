f82210197be70119ca7296fd1195d26c7c7470a5
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-07-12 18:42:44 -0300

    testing pipeline script

--- app/testing_pipeline.py
+++ app/testing_pipeline.py
@@ -7,8 +7,4 @@ urls = today_urls()
 print(urls)
 
 for url in urls:
-    type, content, date = scrape_article(url)
-    print(type)
-    print(content)
-    print(date)
-    print('***********')
+    publication = generate_publication(url)
