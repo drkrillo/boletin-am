3fafbb45460dca5bd4fe58e60ca60a8d2a3a5a93
Juan Pasutti <63800323+drkrillo@users.noreply.github.com>
2023-07-13 01:23:15 -0300

    added loader script

---
+++ etl/utils/loader.py
@@ -0,0 +1,13 @@
+import json
+
+FILE = '../../data.json'
+
+def json_loader(
+        entry: dict,
+        file: str = FILE,
+) -> None:
+    with open(file, "r+") as file:
+        data = json.load(file)
+        data.append(entry)
+        file.seek(0)
+        json.dump(data, file)
